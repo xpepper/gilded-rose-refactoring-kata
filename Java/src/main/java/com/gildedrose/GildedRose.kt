cdc400ebd95c4df6e2ffadd8a2bb987b0cc0455e
Pietro Di Bello <pierodibello@gmail.com>
2023-10-17 12:37:51 +0200

    refactor: move companion object into a top-level function

--- Java/src/main/java/com/gildedrose/GildedRose.kt
+++ Java/src/main/java/com/gildedrose/GildedRose.kt
@@ -4,57 +4,55 @@ class GildedRose(@JvmField var items: Array<Item>) {
     fun updateQuality() {
         for (item in items) update(item)
     }
+}
 
-    companion object {
-        private fun update(item: Item) {
-            val name = item.name
-            if (name != "Aged Brie"
-                    && name != "Backstage passes to a TAFKAL80ETC concert") {
+private fun update(item: Item) {
+    val name = item.name
+    if (name != "Aged Brie"
+        && name != "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.quality > 0) {
+            if (name != "Sulfuras, Hand of Ragnaros") {
+                item.quality = item.quality - 1
+            }
+        }
+    } else {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+
+            if (name == "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+            }
+        }
+    }
+
+    if (name != "Sulfuras, Hand of Ragnaros") {
+        item.sellIn = item.sellIn - 1
+    }
+
+    if (item.sellIn < 0) {
+        if (name != "Aged Brie") {
+            if (name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (item.quality > 0) {
                     if (name != "Sulfuras, Hand of Ragnaros") {
                         item.quality = item.quality - 1
                     }
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-
-                    if (name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                    }
-                }
+                item.quality = item.quality - item.quality
             }
-
-            if (name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn = item.sellIn - 1
-            }
-
-            if (item.sellIn < 0) {
-                if (name != "Aged Brie") {
-                    if (name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.quality > 0) {
-                            if (name != "Sulfuras, Hand of Ragnaros") {
-                                item.quality = item.quality - 1
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
-                    }
-                }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
             }
         }
     }
