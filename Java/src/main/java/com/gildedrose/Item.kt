dc3436cfc280658bc2b837817a15241bf9f03859
Pietro Di Bello <pierodibello@gmail.com>
2023-10-17 12:08:10 +0200

    refactor: convert Items from Java to Kotlin

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -1,21 +1,17 @@
-package com.gildedrose;
+package com.gildedrose
 
-public class Item {
+class Item(name: String?, sellIn: Int, quality: Int) {
+    var name: String? = null
+    var sellIn: Int = 0
+    var quality: Int = 0
 
-    public String name;
-
-    public int sellIn;
-
-    public int quality;
-
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
+    init {
+        this.name = name
+        this.sellIn = sellIn
+        this.quality = quality
     }
 
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
+    override fun toString(): String {
+        return this.name + ", " + this.sellIn + ", " + this.quality
     }
 }
