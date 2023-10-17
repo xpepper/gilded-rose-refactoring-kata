11d63651fface5750585afb255180aca79209143
Pietro Di Bello <pierodibello@gmail.com>
2023-10-17 12:13:10 +0200

    refactor: improve Item Kotlin conversion by applying idiomatic Kotlin conventions

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -1,17 +1,10 @@
 package com.gildedrose
 
-class Item(name: String?, sellIn: Int, quality: Int) {
-    var name: String? = null
-    var sellIn: Int = 0
+class Item(
+    val name: String,
+    var sellIn: Int = 0,
     var quality: Int = 0
+) {
 
-    init {
-        this.name = name
-        this.sellIn = sellIn
-        this.quality = quality
-    }
-
-    override fun toString(): String {
-        return this.name + ", " + this.sellIn + ", " + this.quality
-    }
+    override fun toString() = "${this.name}, ${this.sellIn}, ${this.quality}"
 }
