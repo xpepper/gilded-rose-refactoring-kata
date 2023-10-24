b769528882191805f2a0b311dd901b65c66ee3ca
Pietro Di Bello <pierodibello@gmail.com>
2023-10-24 11:27:29 +0200

    refactor: merge update1 and update3 methods

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -12,26 +12,24 @@ open class BaseItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn
     fun update() {
         age()
         update1()
-        update3()
-    }
-
-    protected open fun update1() {
-        if (quality > 0) {
-            quality = quality - 1
-        }
     }
 
     protected open fun age() {
         sellIn = sellIn - 1
     }
 
-    protected open fun update3() {
+    protected open fun update1() {
+        if (quality > 0) {
+            quality = quality - 1
+        }
         if (sellIn < 0) {
             if (quality > 0) {
                 quality = quality - 1
             }
         }
+
     }
+
 }
 
 class Brie(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
@@ -39,9 +37,6 @@ class Brie(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, qua
         if (quality < 50) {
             quality = quality + 1
         }
-    }
-
-    override fun update3() {
         if (sellIn < 0) {
             if (quality < 50) {
                 quality = quality + 1
@@ -67,17 +62,14 @@ class BackstagePass(name: String, sellIn: Int, quality: Int) : BaseItem(name, se
                 }
             }
         }
-    }
-
-    override fun update3() {
         if (sellIn < 0) {
             quality = 0
         }
     }
+
 }
 
 class Sulfuras(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
-    override fun update1() {}
     override fun age() {}
-    override fun update3() {}
+    override fun update1() {}
 }
