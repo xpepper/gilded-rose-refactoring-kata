8ffdf037d0c4f3119e876aae0c18858332910d7d
Pietro Di Bello <pierodibello@gmail.com>
2024-04-16 13:39:26 +0200

    refactor: move update function out of GildedRose class so that we know it does not rely on its state anymore

--- Java/src/main/java/com/gildedrose/GildedRose.kt
+++ Java/src/main/java/com/gildedrose/GildedRose.kt
@@ -6,55 +6,61 @@ class GildedRose(var items: Array<Item>) {
             update(item)
         }
     }
+}
 
-    private fun update(item: Item) {
-        if (item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert") {
+private fun update(item: Item) {
+    if (isBrie(item) || isPass(item)) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+
+            if (isPass(item)) {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+            }
+        }
+    } else {
+        if (item.quality > 0) {
+            if (isSulfuras(item)) {
+            } else {
+                item.quality = item.quality - 1
+            }
+        }
+    }
+
+    if (isSulfuras(item)) {
+    } else {
+        item.sellIn = item.sellIn - 1
+    }
+
+    if (item.sellIn < 0) {
+        if (isBrie(item)) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1
-
-                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-                }
             }
         } else {
-            if (item.quality > 0) {
-                if (item.name == "Sulfuras, Hand of Ragnaros") {
-                } else {
-                    item.quality = item.quality - 1
-                }
-            }
-        }
-
-        if (item.name == "Sulfuras, Hand of Ragnaros") {
-        } else {
-            item.sellIn = item.sellIn - 1
-        }
-
-        if (item.sellIn < 0) {
-            if (item.name == "Aged Brie") {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                }
+            if (isPass(item)) {
+                item.quality = item.quality - item.quality
             } else {
-                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                    item.quality = item.quality - item.quality
-                } else {
-                    if (item.quality > 0) {
-                        if (item.name == "Sulfuras, Hand of Ragnaros") return
-                        item.quality = item.quality - 1
-                    }
+                if (item.quality > 0) {
+                    if (isSulfuras(item)) return
+                    item.quality = item.quality - 1
                 }
             }
         }
     }
 }
+
+private fun isSulfuras(item: Item) = item.name == "Sulfuras, Hand of Ragnaros"
+
+private fun isPass(item: Item) = item.name == "Backstage passes to a TAFKAL80ETC concert"
+
+private fun isBrie(item: Item) = item.name == "Aged Brie"
