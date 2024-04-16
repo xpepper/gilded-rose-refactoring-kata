617310b252a77f15c2d6c8a6ad31e9d5ea967e24
Pietro Di Bello <pierodibello@gmail.com>
2024-04-16 14:26:41 +0200

    refactor: move age and degrade into ItemType

--- Java/src/main/java/com/gildedrose/GildedRose.kt
+++ Java/src/main/java/com/gildedrose/GildedRose.kt
@@ -10,71 +10,71 @@ class GildedRose(var items: Array<Item>) {
     }
 }
 
-private fun degrade(itemType: ItemType, item: Item) {
-    when (itemType) {
-        BRIE -> {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1
-            }
-            if (item.sellIn < 0) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                }
-            }
-        }
-
-        PASS -> {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1
-
-                if (item.sellIn < 10) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
-                    }
-                }
-                if (item.sellIn < 5) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
-                    }
-                }
-            }
-            if (item.sellIn < 0) {
-                item.quality = item.quality - item.quality
-            }
-        }
-
-
-        SULFURAS -> {
-        }
-
-        OTHER -> {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1
-            }
-            if (item.sellIn < 0) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1
-                }
-            }
-        }
-    }
-}
-
-private fun age(itemType: ItemType, item: Item) {
-    when (itemType) {
-        SULFURAS -> {
-        }
-
-        else -> item.sellIn = item.sellIn - 1
-    }
-}
-
 enum class ItemType {
     SULFURAS, PASS, BRIE, OTHER;
 
     fun update(item: Item) {
-        age(this, item)
-        degrade(this, item)
+        this.age(item)
+        this.degrade(item)
+    }
+
+    private fun ItemType.age(item: Item) {
+        when (this) {
+            SULFURAS -> {
+            }
+
+            else -> item.sellIn = item.sellIn - 1
+        }
+    }
+
+    private fun ItemType.degrade(item: Item) {
+        when (this) {
+            BRIE -> {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+                if (item.sellIn < 0) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+            }
+
+            PASS -> {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+
+                    if (item.sellIn < 10) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
+                    }
+                    if (item.sellIn < 5) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
+                    }
+                }
+                if (item.sellIn < 0) {
+                    item.quality = item.quality - item.quality
+                }
+            }
+
+
+            SULFURAS -> {
+            }
+
+            OTHER -> {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1
+                }
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1
+                    }
+                }
+            }
+        }
     }
 }
 
