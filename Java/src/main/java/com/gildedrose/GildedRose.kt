e649f9a852e034a5f0f5fa6200e9bdd1c2b21f6c
Pietro Di Bello <pierodibello@gmail.com>
2024-04-16 14:21:10 +0200

    refactor: move up degradation in a separate when branch

--- Java/src/main/java/com/gildedrose/GildedRose.kt
+++ Java/src/main/java/com/gildedrose/GildedRose.kt
@@ -13,11 +13,16 @@ class GildedRose(var items: Array<Item>) {
 private fun update(item: Item) {
     val itemType = typeOf(item)
     when (itemType) {
+        SULFURAS -> {
+        }
+        else -> item.sellIn = item.sellIn - 1
+    }
+
+    when (itemType) {
         BRIE -> {
             if (item.quality < 50) {
                 item.quality = item.quality + 1
             }
-            item.sellIn = item.sellIn - 1
             if (item.sellIn < 0) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1
@@ -29,19 +34,17 @@ private fun update(item: Item) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1
 
-                if (item.sellIn < 11) {
+                if (item.sellIn < 10) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1
                     }
                 }
-
-                if (item.sellIn < 6) {
+                if (item.sellIn < 5) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1
                     }
                 }
             }
-            item.sellIn = item.sellIn - 1
             if (item.sellIn < 0) {
                 item.quality = item.quality - item.quality
             }
@@ -55,7 +58,6 @@ private fun update(item: Item) {
             if (item.quality > 0) {
                 item.quality = item.quality - 1
             }
-            item.sellIn = item.sellIn - 1
             if (item.sellIn < 0) {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1
