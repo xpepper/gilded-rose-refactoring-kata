ba1c300a333007bc3c2eace7439462cb7e5a0ace
Pietro Di Bello <pierodibello@gmail.com>
2024-04-16 14:01:33 +0200

    refactor: push down item.sellIn < 0

--- Java/src/main/java/com/gildedrose/GildedRose.kt
+++ Java/src/main/java/com/gildedrose/GildedRose.kt
@@ -56,27 +56,31 @@ private fun update(item: Item) {
         }
     }
 
-    if (item.sellIn < 0) {
-        when (itemType) {
-            BRIE -> {
+    when (itemType) {
+        BRIE -> {
+            if (item.sellIn < 0) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1
                 }
             }
+        }
 
-            PASS -> {
+        PASS -> {
+            if (item.sellIn < 0) {
                 item.quality = item.quality - item.quality
             }
+        }
 
-            SULFURAS -> {
-            }
+        SULFURAS -> {
+        }
 
-            OTHER -> {
+        OTHER -> {
+            if (item.sellIn < 0) {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1
                 }
-
             }
+
         }
     }
 }
